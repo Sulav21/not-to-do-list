df82187060830aa199d7e75ee45db14fca9e9014
Sulav Adhikari <sulavadhikari17@gmail.com>
2022-02-15 10:57:30 +1100

    third commit

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ const taskList = []
 const badList=[]
 const hrPerWeek = 168 
 
+const total = taskList.reduce((subttl,item)=>{
+   return subttl + item.hr
+   },0)
+
 const handleOnSubmit = (e) => {
+   
 const frmData = new FormData(e) 
 const task= frmData.get("task")
 const hr= +frmData.get("hr")
-
+if(hr<1) return alert("Enter a positive value")
+const ttlBadHrs =totalTimeSaved()
+const total = taskList.reduce((subttl,item)=>{
+   return subttl + item.hr
+   },0) + hr 
+   if((ttlBadHrs+total) >hrPerWeek){
+      return alert("You have exceeded maximum number of hours per week")
+   }
 const obj = {
    task,
    hr, 
@@ -28,22 +40,63 @@ str += `
 <td>${item.hr}hr</td>
 <td class="text-end"><button class="bg-transparent" onclick="deleteItem(${i})"><i class="fa-solid fa-trash" title="Delete"></i></button>
 
-<button class="btn btn-success"><i class="fa-solid fa-arrow-right" title="Mark as bad list"></i>
+<button class="btn btn-success" onclick="markAsNotToDo(${i})"><i class="fa-solid fa-arrow-right" title="Mark as bad list"></i>
 </button>
 </td>
 </tr>
 
 `
+
 })
 
 document.getElementById("task-list").innerHTML = str
 
 }
 
+const displayBadList=()=>{
+   let str = ""
+  badList.map((item,i)=>{
+   str += `
+   <tr>
+   <td scope="row"><input type="checkbox" name="" id="" /></td>
+   <td>${item.task}</td>
+   <td>${item.hr}hr</td>
+   <td class="text-end">
+   <button class="btn btn-success" onclick="markAsGoodList(${i})"><i class="fa-solid fa-arrow-left" title="Mark as good list"></i>
+   </button>
+   <button class="bg-transparent" onclick="deleteItem1(${i})"><i class="fa-solid fa-trash" title="Delete"></i></button>
+   
+   
+   </button>
+   </td>
+   </tr>
+   
+   `
+   
+   })
+   
+   document.getElementById("bad-list").innerHTML = str
+   
+   }
+
+
+
 const deleteItem = (i)=>{
+   if(!confirm("Are you sure you want to delete the item ?")){
+     return
+   }
   taskList.splice(i,1)
-  
   display()
+  totalTaskHours()
+}
+
+const deleteItem1 = (i)=>{
+   if(!confirm("Are you sure you want to delete the item ?")){
+     return
+   }
+  badList.splice(i,1)
+  displayBadList()
+  totalTimeSaved()
   totalTaskHours()
 }
 
@@ -51,9 +104,38 @@ const totalTaskHours=()=>{
     const total = taskList.reduce((subttl,item)=>{
     return subttl + item.hr
     },0)
-    console.log(total)
+    const ttlBadHrs =totalTimeSaved()
 
-   document.getElementById("totalHours").innerHTML = total 
+    const totalHours = total + ttlBadHrs
+  
+
+   document.getElementById("totalHours").innerHTML = totalHours
 
 }
 
+const totalTimeSaved=()=>{
+   const ttlBadHrs =badList.reduce((subttl,item)=>subttl+item.hr,0)
+
+
+  document.getElementById("totalhourssaved").innerHTML = ttlBadHrs
+  return ttlBadHrs
+
+}
+
+
+const markAsNotToDo = i =>{
+   const itm = taskList.splice(i,1)
+   display()
+   badList.push(itm[0])
+   displayBadList()
+   totalTimeSaved()
+
+}
+
+const markAsGoodList = i =>{
+const itm1 = badList.splice(i,1)
+displayBadList()
+taskList.push(itm1[0])
+display()
+totalTimeSaved()
+}
